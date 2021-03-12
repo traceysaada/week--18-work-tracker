646e57bef63b92b78d721fb1afdeee2a829eb0e0
Tracey Saada <tracey.saada@outlook.com>
2021-03-12 17:51:20 +0000

    Fixed spelling in view and updated create workout

--- routes/api.js
+++ routes/api.js
@@ -3,25 +3,24 @@ const Workout = require("../models/workout.js");
 //require in expressrouter
 const router = require("express").Router()
 //post request to create workouts
-router.post("/api/workout", ({body}, res) => {
- Workout.create(body)
+router.post("/api/workouts", (req, res) => {
+ Workout.create({})
  .then(dbWorkout => {
      res.json(dbWorkout);
  })
  .catch(err => {
      res.status(400).json(err);
-
- });
+ })
 });
 //put request to update a workout
-router.put("/api/workout/:id", function(req, res){
+router.put("/api/workouts/:id", function(req, res){
     db.Workout.upDateOne({_id: req.params._id}, {rating: req.body.rating}).then(function(dbWorkout){
         res.json(dbWorkout);
     })
 },
 
 //get request to get workouts out of database
-router.get("/api/workout", (req, res) => {
+router.get("/api/workouts", (req, res) => {
     Workout.findOne().then(dbworkout => {
         res.json(dbworkout);
     })
@@ -30,7 +29,7 @@ router.get("/api/workout", (req, res) => {
     })
 }),
 //get request to get a range of workouts out of the database
-router.get("/api/workout", (req, res) => 
+router.get("/api/workouts", (req, res) => 
 Workout.findAll().then(dbworkout => {
     res.json(dbworkout);
 })
--- routes/view.js
+++ routes/view.js
@@ -3,8 +3,8 @@ const router = require("express").Router();
 //require in the path npm package
 const path = require("path");
 //get require which is going to return the exercise.html when the user visits /exercise
-router.get('/excercise', (req, res) =>
-res.sendFile(path.join(__dirname, '../public/excercise.html'))
+router.get('/exercise', (req, res) =>
+res.sendFile(path.join(__dirname, '../public/exercise.html'))
 );
 //get request which is going to return the stats.html when the user visits /stats
 router.get('/stats', (req, res) =>
